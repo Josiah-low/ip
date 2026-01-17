61f7c76c0ec911662c4f35cc58f4d6e4ec12a9d8
Josiah-low <e1397806@u.nus.edu>
2026-01-17 18:02:05 +0800

    Add support for marking tasks as done/undone

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,6 +2,7 @@ public class Task {
     private String name;
     private int index;
     private static int numOfTasks = 0;
+    private boolean isDone;
 
     public Task(String name) {
         this.name = name;
@@ -23,5 +24,17 @@ public class Task {
 
     public static int getNumOfTasks() {
         return numOfTasks;
+    }
+
+    public boolean isDone() {
+        return this.isDone;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
     }
 }
