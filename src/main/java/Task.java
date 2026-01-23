e3c8dfb3589d98b9c765ba5adac7ce8c5346393e
Josiah-low <e1397806@u.nus.edu>
2026-01-23 09:37:52 +0800

    Add Javadoc comments for Task class

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,7 +1,19 @@
-public abstract class Task {
+/**
+ * Represents an abstract task in Cove's task system.
+ * This is the base class for all task types. All tasks have a description
+ * and a completion status. Subclasses must implement specific task types.
+ */
+ public abstract class Task {
+    /** Description of the task */
     private String description;
+    /** Completion status of the task */
     private boolean isDone;
 
+    /**
+     * Creates a new Task with the specified description.
+     *
+     * @param description The description of the task.
+     */
     public Task(String description) {
         this.description = description;
     }
@@ -18,6 +30,11 @@ public abstract class Task {
         this.isDone = isDone;
     }
 
+    /**
+     * Returns a string representation of a Task for printing to console.
+     * The format is: "[isDone] description" where isDone is "X" if true,
+     * or a space if false.
+     */
     @Override
     public String toString() {
         if (this.isDone) {
@@ -27,6 +44,10 @@ public abstract class Task {
         }
     }
 
+    /**
+     * Returns a string representation of a Task for saving to the data file.
+     * The format is: "X|description" where X is "1" if true, or "0" if false.
+     */
     public String dataString() {
         if (this.isDone) {
             return "1|" + this.description;
