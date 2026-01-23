3d46e213305e46f11d073838c42e7a8a47e0135d
Josiah-low <e1397806@u.nus.edu>
2026-01-23 19:11:12 +0800

    Update some TaskList class methods to use taskIndex

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -16,24 +16,24 @@ public class TaskList {
         return this.tasks;
     }
 
-    public void addTask(Task task) {
-        this.tasks.add(task);
-    }
-
     public int size() {
         return this.tasks.size();
     }
 
-    public void deleteTask(Task task) {
-        this.tasks.remove(task);
+    public void addTask(Task task) {
+        this.tasks.add(task);
     }
 
-    public void markTask(Task task) {
-        task.setDone(true);
+    public void deleteTask(int taskIndex) {
+        this.tasks.remove(taskIndex - 1);
     }
 
-    public void unmarkTask(Task task) {
-        task.setDone(false);
+    public void markTask(int taskIndex) {
+        this.tasks.get(taskIndex - 1).setDone(true);
+    }
+
+    public void unmarkTask(int taskIndex) {
+        this.tasks.get(taskIndex - 1).setDone(false);
     }
 
 }
