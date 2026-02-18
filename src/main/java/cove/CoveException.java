44860296d727f01b3b39c70a777597fd5e078372
Josiah-low <e1397806@u.nus.edu>
2026-02-18 15:21:45 +0800

    Update Javadoc comments for CoveException class

--- src/main/java/cove/CoveException.java
+++ src/main/java/cove/CoveException.java
@@ -1,17 +1,18 @@
 package cove;
 
 /**
- * Custom exception class for errors in the cove.Cove chatbot.
+ * Custom exception class for errors in the Cove chatbot.
  * This exception is thrown when user input is invalid, or
  * when other errors occur. The exception message is formatted
  * with a spacing and a separator line for consistent message
  * display in the console.
  */
 public class CoveException extends Exception {
+
     /**
      * Creates a new cove.CoveException with the specified error message.
      * The message is automatically formatted with a space in front and
-     * a separator line at the end for consistency with cove.Cove's message style.
+     * a separator line at the end for consistency with Cove's message style.
      *
      * @param message The error message to display in the console.
      */
